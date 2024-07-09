b5b68fabc71320fa20f172b0818203176d872ade
Peter Aisthorpe <61685225+PeterAisthorpe@users.noreply.github.com>
2024-07-09 11:38:24 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] harryPotterFilms = {
+    "Harry Potter and the Philosophers Stone",
+    "Harry Potter and the Chamber of Secrets",
+    "Harry Potter and the Prisoner of Azkaban",
+    "Harry Potter and the Goblet of Fire",
+    "Harry Potter and the Order of Phoenix",
+    "Harry Potter and the Half Blood Prince",
+    "Harry Potter and the Deathly Hallows Part One",
+    "Harry Potter and the Deathly Hallows Part Two"
+};
+
+Random random = new Random();
+
+int filmChoice = random.Next(harryPotterFilms.Length);
+
+string chosenFilm = harryPotterFilms[filmChoice];
+Console.WriteLine($"The chosen film is{chosenFilm}");
